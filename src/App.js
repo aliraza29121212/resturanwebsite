97e87587d9609b2164b9ba24acc572d66224102e
Ali-raza <you@example.com>
2023-01-16 21:41:01 +0500

    ad

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Resturant from "./component/Basics/Resturant";
 import Todo from "./component/Todo/Todo";
+import Temp from "./component/Weather/Temp";
 
 const App = () => {
   return (
     <div className="" style={{ padding: " 0px 60px" }}>
       {/* <Resturant /> */}
-      <Todo />
+      {/* <Todo /> */}
+      <Temp />
     </div>
   );
 };
---
+++ src/component/Weather/Temp.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from "react";
+import "./style.css";
+
+const Temp = () => {
+  const [searchValue, setSearchValue] = useState("lahore");
+
+  // input function
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=7ce9978b0d99ad827c6180480ad1ae6b`;
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="Search"
+            placeholder="Search here"
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+          <button
+            className="searchButton"
+            type="button"
+            onClick={getWeatherInfo}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      <article className="widget">
+        <div className="weatherIcon">
+          <i className={"wi wi-day-sunny"}></i>
+        </div>
+        <div className="weatherInfo">
+          <div className="temperature">
+            <span>25.5deg</span>
+          </div>
+          <div className="description">
+            <div className="weatherCondition">sunny</div>
+            <div className="place">Lahore, India</div>
+          </div>
+        </div>
+        <div className="date">{new Date().toLocaleString()}</div>
+        <div className="extra-temp">
+          <div className="temp-info-minmax">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-sunset"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                19:19 PM <br />
+                Sunset
+              </p>
+            </div>
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-humidity"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                19:19 PM <br />
+                Humidity
+              </p>
+            </div>
+          </div>
+          <div className="weather-extra-info">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-rain"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                19:19 PM <br />
+                Pressure
+              </p>
+            </div>
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-strong-wind"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                19:19 PM <br />
+                Speed
+              </p>
+            </div>
+          </div>
+        </div>
+      </article>
+    </>
+  );
+};
+
+export default Temp;
